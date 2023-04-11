0010ae7f3c6481a82516e33d55d4a52cc17bbe81
YouCheng Lin <youchenglin0304@gmail.com>
2023-04-11 14:59:42 +0800

    ZeroJudge a044 v1.0

---
+++ ZeroJudge/Basic Question Bank/a/a044.py
@@ -0,0 +1,12 @@
+"""
+Problem: https://zerojudge.tw/ShowProblem?problemid=a044
+Title: 空間切割
+"""
+
+while True:
+    try:
+        n = int(input())
+    except EOFError:
+        break
+
+    print((n**3 + 5 * n + 6) // 6)
